1ba8b75a72e22ab0d00ae8cf7baa3c387a25555c
Charlie Strawn <cdstrawn@gmail.com>
2019-04-18 06:38:42 -0500

    csharp: Start on publisher example

---
+++ examples/csharp/NatsPublisher.cs
@@ -0,0 +1,25 @@
+﻿using frugal.Protocol;
+using frugal.Provider;
+using frugal.Transport;
+using NATS.Client;
+using Thrift.Protocol;
+
+namespace frugal_example
+{
+    class NatsPublisher
+    {
+        static void Main(string[] args)
+        {
+            var fProtocolFactory = new FProtocolFactory(new TBinaryProtocol.Factory());
+            var cf = new ConnectionFactory();
+            IConnection conn = cf.CreateConnection();
+
+            var publisherFactory = FNatsPublisherTransport.Factory(conn);
+            var subscriberFactory = FNatsSubscriberTransport.Factory(conn);
+
+            var provider = new FScopeProvider(publisherFactory, subscriberFactory, fProtocolFactory);
+
+
+        }
+    }
+}
