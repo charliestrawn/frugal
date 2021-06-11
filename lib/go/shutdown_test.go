1dd8cc08c5333b0a5ebdc47aea58f144497d42a2
Nate Woods <nate.woods@workiva.com>
2021-06-11 18:22:38 -0500

    tracing received messages

--- lib/go/shutdown_test.go
+++ lib/go/shutdown_test.go
@@ -2,26 +2,30 @@ package frugal
 
 import (
 	"fmt"
+	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/apache/thrift/lib/go/thrift"
 	"github.com/nats-io/nats.go"
+	"github.com/stretchr/testify/assert"
 )
 
 type testProcessor struct {
+	count *int32
 }
 
 func (*testProcessor) AddMiddleware(ServiceMiddleware)           {}
 func (*testProcessor) Annotations() map[string]map[string]string { return nil }
 func (proc *testProcessor) Process(in, out *FProtocol) error {
+	atomic.AddInt32(proc.count, 1)
 	fmt.Printf("Getting request: %v", in)
 	time.Sleep(time.Second)
 	return nil
 }
 
 func TestShutdown(t *testing.T) {
-	const REQ = 100
+	const REQ = 10
 
 	conn, err := nats.Connect(nats.DefaultURL)
 	if err != nil {
@@ -29,14 +33,15 @@ func TestShutdown(t *testing.T) {
 		panic(err)
 	}
 
-	proc := &testProcessor{}
+	start := int32(0)
+	proc := &testProcessor{count: &start}
 
 	srv := NewFNatsServerBuilder(
 		conn,
 		proc,
 		NewFProtocolFactory(thrift.NewTJSONProtocolFactory()),
 		[]string{"frugal.test.shutdown"},
-	).WithWorkerCount(2).WithQueueLength(100).Build()
+	).WithWorkerCount(2).WithQueueLength(REQ).Build()
 
 	go func() {
 		err := srv.Serve()
@@ -47,7 +52,7 @@ func TestShutdown(t *testing.T) {
 
 	// TODO: publish a bunch of messages
 	for i := 0; i < REQ; i++ {
-		err := conn.PublishRequest("frugal.test.shutdown", "_INBOX.round-file", []byte("work"))
+		err := conn.PublishRequest("frugal.test.shutdown", "INBOX.round-file", []byte("work"))
 		if err != nil {
 			panic(err)
 		}
@@ -57,4 +62,6 @@ func TestShutdown(t *testing.T) {
 	if err = srv.Stop(); err != nil {
 		panic(err)
 	}
+
+	assert.Equal(t, atomic.LoadInt32(proc.count), int32(REQ))
 }
