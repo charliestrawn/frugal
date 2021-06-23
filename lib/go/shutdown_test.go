fa17d9b5f0889453e4aeef4b4df07561b7ce56af
Nate Woods <nate.woods@workiva.com>
2021-06-22 23:41:11 -0500

    successfully demonstrating lost work during shutdown

--- lib/go/shutdown_test.go
+++ lib/go/shutdown_test.go
@@ -1,7 +1,7 @@
 package frugal
 
 import (
-	"fmt"
+	"runtime"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -19,23 +19,27 @@ func (*testProcessor) AddMiddleware(ServiceMiddleware)           {}
 func (*testProcessor) Annotations() map[string]map[string]string { return nil }
 func (proc *testProcessor) Process(in, out *FProtocol) error {
 	atomic.AddInt32(proc.count, 1)
-	fmt.Printf("Getting request: %v", in)
-	time.Sleep(time.Second)
+	time.Sleep(time.Millisecond * 100)
 	return nil
+}
+
+func safe(tb testing.TB, err error) {
+	if err != nil {
+		tb.Fatalf("Failed: %v", err)
+	}
 }
 
 func TestShutdown(t *testing.T) {
 	const REQ = 10
-
-	conn, err := nats.Connect(nats.DefaultURL)
-	if err != nil {
-		t.Log("docker run --rm -it -p 4222:4222 nats")
-		panic(err)
-	}
-
 	start := int32(0)
 	proc := &testProcessor{count: &start}
 
+	// Startup a generic nats processor
+	conn, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		t.Log("docker run --rm -it -p 4222:4222 nats")
+		t.Skipf("Unable to connect to nats: %v", err)
+	}
 	srv := NewFNatsServerBuilder(
 		conn,
 		proc,
@@ -43,25 +47,29 @@ func TestShutdown(t *testing.T) {
 		[]string{"frugal.test.shutdown"},
 	).WithWorkerCount(2).WithQueueLength(REQ).Build()
 
+	// Start the server in the background and let us know when it exits
+	serveDone := make(chan bool)
 	go func() {
-		err := srv.Serve()
-		if err != nil {
-			panic(err)
-		}
+		safe(t, srv.Serve())
+		close(serveDone)
 	}()
+	time.Sleep(time.Millisecond * 10)
+	runtime.Gosched() // wait for server to startup
 
-	// TODO: publish a bunch of messages
+	// Publish a bunch of messages (ignore frugal stack, just trigger nats processors)
 	for i := 0; i < REQ; i++ {
-		err := conn.PublishRequest("frugal.test.shutdown", "INBOX.round-file", []byte("work"))
-		if err != nil {
-			panic(err)
-		}
+		safe(t, conn.PublishRequest("frugal.test.shutdown", "INBOX.round-file", []byte("work")))
 	}
-	time.Sleep(time.Second)
+	safe(t, conn.Flush())
+	time.Sleep(time.Millisecond * 10)
+	runtime.Gosched() // wait for some form of processing
 
-	if err = srv.Stop(); err != nil {
-		panic(err)
-	}
+	// Shutdown server and wait for Serve to return
+	safe(t, srv.Stop())
+	<-serveDone
 
-	assert.Equal(t, atomic.LoadInt32(proc.count), int32(REQ))
+	// Make some assertions about the state of things
+	count := atomic.LoadInt32(proc.count)
+	t.Logf("Received %d; Processed: %d; Remaining: %d", REQ, count, len(srv.(*fNatsServer).workC))
+	assert.Equal(t, count, int32(REQ))
 }
