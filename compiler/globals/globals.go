c916af2138ffe18307a65a89f0ad7db2ddc90369
bender-wk <bender@workiva.com>
2022-11-04 17:05:33 +0000

    RELEASE Frugal v3.16.6

--- compiler/globals/globals.go
+++ compiler/globals/globals.go
@@ -21,7 +21,7 @@ import (
 )
 
 // Version of the Frugal compiler.
-const Version = "3.16.5"
+const Version = "3.16.6"
 
 // Global variables.
 var (
--- lib/python/frugal/version.py
+++ lib/python/frugal/version.py
@@ -10,4 +10,4 @@
 # limitations under the License.
 
 
-__version__ = '3.16.5'
+__version__ = '3.16.6'
